d7d056c870108fd7ebc9b4f44759bcb81a326ff9
DuhaAbdAl <114020564+DuhaAbdAl@users.noreply.github.com>
2023-01-19 23:24:13 +0200

    19/1/2023

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet, Text, View, Pressable, Image,TouchableOpacity } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, Pressable, Image, TouchableOpacity, Dimensions } from "react-native";
 import React from "react";
 import { ImageHeaderScrollView, TriggeringView } from 'react-native-image-header-scroll-view';
 import { data } from "../mokData/data";
@@ -13,8 +13,7 @@ const Info = (props) => {
     const drawerNavigation = navigation.getParent('LeftDrawer');
     const { name } = props.route.params || {}
     const found = data.find(element => element.title == name);
-    // const foundImg = data.find(element => element.image == image);
-    // console.log('foundImg', foundImg)
+
     const renderData = () => {
         var keys = Object.keys(found?.info || {});
         return keys.map(key => {
@@ -27,28 +26,26 @@ const Info = (props) => {
         })
     }
     // console.log('found.image', found.image)
-    // console.log('found.title', found.title)
+    console.log('found.title', found.title)
     const HeaderImage = (props) => {
         const imgeTitle = useRef(null)
         return (
             <ImageHeaderScrollView
-                maxHeight={250}
-                minHeight={55}
-                maxOverlayOpacity={0.5}
+                maxHeight={MAX_HEIGHT}
+                minHeight={MIN_HEIGHT}
+                maxOverlayOpacity={0.6}
                 minOverlayOpacity={0.3}
-                // headerImage={require('../assets/images/FlowersPage.jpeg')}
+                fadeOutForeground
                 renderHeader={() => {
-                  return  <Image style={{resizeMode:'repeat'}} source={found.image} /> 
+                    return <Image style={styles.image} source={found.image} />
                 }}
                 renderForeground={() => (
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-
-
+                    <View style={styles.Foreground}>
                         <Text style={styles.titleAnimatedUp}> {found.title} </Text>
                     </View>
                 )}
                 renderFixedForeground={() => (
-                    <Animatable.View ref={imgeTitle} style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Animatable.View ref={imgeTitle} style={styles.FixedForeground}>
                         <Pressable onPress={() => {
                             navigation.goBack();
                         }}>
@@ -56,7 +53,7 @@ const Info = (props) => {
                                 name="chevron-back"
                                 size={30}
                                 color="white"
-                                style={{ marginLeft: 15, marginTop: 12, alignSelf: 'flex-start' }} />
+                                style={styles.icon} />
                         </Pressable>
                         <Text style={styles.titleAnimated}>{found.title}</Text>
                         <TouchableOpacity onPress={() => {
@@ -72,45 +69,46 @@ const Info = (props) => {
             >
 
                 <TriggeringView
-                 onHide={() => imgeTitle.current.fadeInUp(200)}
-                 onDisplay={() => imgeTitle.current.fadeOut(100)}
+                    onHide={() => imgeTitle.current.fadeInUp(200)}
+                    onDisplay={() => imgeTitle.current.fadeOut(100)}
                 >
-                    <ImageBackground
-                        style={styles.imageBack}
-                        source={require('../assets/images/info10.jpeg')} >
-                        {renderData()}
-                    </ImageBackground>
+                    {/* <Text style={styles.titleText}> */}
+                        <Text style={styles.name}>{found.title}</Text> 
+                    {/* </Text> */}
                 </TriggeringView>
+                <ImageBackground
+                    style={styles.imageBack}
+                    source={require('../assets/images/info10.jpeg')} >
+                    {renderData()}
+                </ImageBackground>
                 {props.children}
             </ImageHeaderScrollView >
         )
     }
 
     return (
-        // <ImageBackground
-        //         style={styles.imageBack}
-        //         source={require('../assets/images/info10.jpeg')} >
         <HeaderImage style={styles.headerImage} />
-
-    //             {/* <ScrollView> */ }
-    // {/* {renderData()} */ }
-    // {/* </ScrollView> */ }
-
-            // </ImageBackground>
-
     )
 
 }
+const MIN_HEIGHT = 350;
+const MAX_HEIGHT = 250;
+
 const styles = StyleSheet.create({
     headerImage: {
         flex: 1,
         height: 200,
         width: 200,
     },
+    icon: {
+        marginLeft: 15,
+        marginTop: 12,
+        alignSelf: 'flex-start',
+    },
     image: {
-        height: 55,
-        width: '100%',
-        alignSelf: 'center',
+        height: MAX_HEIGHT,
+        width: Dimensions.get('window').width,
+        alignSelf: 'stretch',
         resizeMode: 'cover',
     },
     imageBack: {
@@ -146,29 +144,52 @@ const styles = StyleSheet.create({
         height: 40,
         width: '90%',
     },
+    FixedForeground: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: MIN_HEIGHT,
+        alignItems: 'center',
+        paddingTop: 16,
+        opacity: 0,
+    },
     titleAnimated: {
         fontSize: 30,
         fontWeight: 'italic',
         color: 'white',
-        backgroundColor:'rgba(250, 250, 250, 0.55)',
-        borderRadius:10,
-        height:70,
-        padding:5,
-        textAlign: 'center',
-        marginBottom: 12,
-        marginTop:15 ,
-        paddingTop: 5,
+        backgroundColor: 'rgba(250, 250, 250, 0.55)',
+        // borderRadius: 10,
+        // height: 70,
+        // padding: 5,
+        // textAlign: 'center',
+        // marginBottom: 12,
+        // marginTop: 15,
+        // paddingTop: 5,
+    },
+    Foreground: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flex: 1,
+        alignSelf: 'stretch',
+        alignItems: 'center',
     },
     titleAnimatedUp: {
         fontSize: 30,
         fontWeight: 'italic',
         color: 'green',//transparent
-        textAlign: 'center',
-        marginRight: 170,
-        marginLeft: 165,
-        marginBottom: 10,
-        marginTop: 8,
-        paddingTop: 5,
+        backgroundColor: 'transparent',
+        // textAlign: 'center',
+        // marginRight: 170,
+        // marginLeft: 165,
+        // marginBottom: 10,
+        // marginTop: 8,
+        // paddingTop: 5,
+    },
+    // titleText: {
+    //     fontSize: 20,
+    // },
+    name: {
+        fontSize: 20,
+        fontWeight: 'bold',
     },
     profile: {
         alignSelf: 'flex-end',
