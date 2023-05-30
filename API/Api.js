f31637176ed9e66ae5733da39c03681def7eb586
DuhaAbdAl <114020564+DuhaAbdAl@users.noreply.github.com>
2023-05-30 20:29:17 +0300

    Create Api.js

---
+++ API/Api.js
@@ -0,0 +1,29 @@
+
+// const Base_URL = "https://rosa-verde-lqyr.onrender.com";
+
+// const AppFetch = () => {
+//     fetch(Base_URL, {
+//   method: 'GET',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     firstParam: 'yourValue',
+//     secondParam: 'yourOtherValue',
+//   }),
+// });
+// }
+
+// export const getAllCategory = () => {
+
+//     return fetch("https://rosa-verde-lqyr.onrender.com/Categories.json")
+//         .then(response => response.json())
+//         .then(json => {
+//             return json.categories;
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         });
+
+// };
