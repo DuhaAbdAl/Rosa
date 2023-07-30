4b30679e95eaf94f3da58f04de47a4fd68d1adbc
DuhaAbdAl <114020564+DuhaAbdAl@users.noreply.github.com>
2023-07-30 23:40:33 +0300

    Update Cards.js

--- src/assets/Cards.js
+++ src/assets/Cards.js
@@ -15,6 +15,8 @@ import FontAwesome from "react-native-vector-icons/FontAwesome";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect } from 'react';
 import RosaContext from '../../Store/RosaContext';
+import LinearGradient from 'react-native-linear-gradient';
+import IconContainer from '../Componants/IconContainer';
 
 
 const Cards = (props) => {
@@ -47,27 +49,39 @@ const Cards = (props) => {
 
 
     return (
-        <View style={styles.card} >
-            <Image style={styles.img} source={image} />
-            <Text style={styles.cardText}>{Name}</Text>
-            <View style={styles.icons}>
-                <Pressable onPress={OnClickFavorite} >
-                    <FontAwesome
-                        name={(favo) ? "heart" : "heart-o"}
-                        color={(favo) ? "red" : "white"}
-                        size={25} />
-                </Pressable>
-                <Pressable onPress={() => {
-                    navigation.navigate(ScreenName.CardInfo, { name: Name });
-                }}>
-                    <Fontisto
-                        name='arrow-right-l'
-                        color={"white"}
-                        size={25}
-                    />
-                </Pressable>
-            </View>
-        </View>
+        // <View>
+           <LinearGradient
+                colors={['#276221', '#5bb450']}
+                style={styles.card}
+            >
+             {/* <View style={styles.card} > */}
+                <Image style={styles.img} source={image} />
+                <Text style={styles.cardText}>{Name}</Text>
+                <View style={styles.icons}>
+                    <Pressable onPress={OnClickFavorite} >
+                        <FontAwesome
+                            name={(favo) ? "heart" : "heart-o"}
+                            color={(favo) ? "red" : "white"}
+                            size={25} />
+                    </Pressable>
+                    <Pressable onPress={() => {
+                        navigation.navigate(ScreenName.CardInfo, { name: Name });
+                    }}>
+                        <FontAwesome
+                            name='long-arrow-right'
+                            color={"white"}
+                            size={28}
+                        />
+                    </Pressable>
+                </View>
+            {/* </View> */}
+             </LinearGradient> 
+
+        //     <View>
+        //         <IconContainer />
+        //     </View>
+        //  </View>
+
     )
 }
 const styles = StyleSheet.create({
@@ -75,11 +89,12 @@ const styles = StyleSheet.create({
         flex: 1,
         height: 160,
         flexDirection: "row",
-        backgroundColor: "green",
         marginBottom: 10,
         borderBottomLeftRadius: 40,
         borderTopLeftRadius: 40,
-        justifyContent: "space-between"
+        justifyContent: "space-between",
+        elevation: 10,
+        backgroundColor:"green"
     },
     icons: {
         marginRight: 10,
